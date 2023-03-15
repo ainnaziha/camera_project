fbec06b11a42334c8f2820e4ea0acf5a6f53e2ea
Ain Naziha <jian@Ains-MacBook-Air.local>
2023-03-15 19:52:29 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  List<CameraDescription> cameras = [];
+  try {
+    cameras = await availableCameras();
+  } catch (e) {
+    //log error
+  }
+  runApp(MyApp(cameras: cameras));
+}
+
+class MyApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  const MyApp({super.key, required this.cameras});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: MyHomePage(cameras: cameras),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const MyHomePage({super.key, required this.cameras});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CameraController _cameraController;
+  bool _isRearCameraSelected = true;
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera(widget.cameras[0]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Stack(children: [
+            (_cameraController.value.isInitialized)
+                ? CameraPreview(_cameraController)
+                : Container(
+                color: Colors.black,
+                child: const Center(child: CircularProgressIndicator())),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 150,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                      color: Colors.black),
+                  child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    Expanded(
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          iconSize: 30,
+                          icon: Icon(
+                              _isRearCameraSelected
+                                  ? CupertinoIcons.switch_camera
+                                  : CupertinoIcons.switch_camera_solid,
+                              color: Colors.white),
+                          onPressed: () {
+                            setState(
+                                    () => _isRearCameraSelected = !_isRearCameraSelected);
+                            initCamera(widget.cameras[_isRearCameraSelected ? 0 : 1]);
+                          },
+                        )),
+                    Expanded(
+                        child: GestureDetector(
+                          onTap: takePicture,
+                          child: SizedBox(
+                            height: 80,
+                            width: 80,
+                            child: Stack(
+                              children: const [
+                                Center(
+                                  child: Icon(Icons.circle_outlined, color: Colors.white, size: 80),
+                                ),
+                                Center(
+                                  child: Icon(Icons.circle, color: Colors.white, size: 50),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                    ),
+                    const Spacer(),
+                  ]),
+                )),
+          ]),
+        ));
+  }
+
+  Future takePicture() async {
+    if (!_cameraController.value.isInitialized) {
+      return null;
+    }
+    if (_cameraController.value.isTakingPicture) {
+      return null;
+    }
+    try {
+      await _cameraController.setFlashMode(FlashMode.off);
+      XFile picture = await _cameraController.takePicture();
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
+
+  Future initCamera(CameraDescription cameraDescription) async {
+    _cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+    try {
+      await _cameraController.initialize().then((_) {
+        if (!mounted) return;
+        setState(() {});
+      });
+    } on CameraException catch (e) {
+      debugPrint("camera error $e");
+    }
+  }
+}
